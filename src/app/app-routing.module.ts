8e10c21a3f398a57736552e0063b50a5861c92c3
Basit <devxbasit@gmail.com>
2024-06-10 09:49:13 +0530

    Added core components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosModule } from './todos/todos.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), TodosModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
+import { TodoFooterComponent } from './components/todo-footer/todo-footer.component';
+import { TodoMainComponent } from './components/todo-main/todo-main.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodosRoutingModule } from './todos-routing.module';
+
+@NgModule({
+  declarations: [
+    TodoHeaderComponent,
+    TodoFooterComponent,
+    TodoMainComponent,
+    TodoComponent,
+    TodoListComponent,
+  ],
+  imports: [CommonModule],
+  exports: [TodosRoutingModule],
+})
+export class TodosModule {}
